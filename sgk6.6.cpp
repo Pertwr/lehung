2d8c7e96975af501feb2d87dc7396474d78a7dd8
Pertwr <HungLQTH2207034@fpt.edu.vn>
2022-08-19 13:50:41 +0700

    lequyhung3101

---
+++ sgk6.6.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+   printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+}
---
+++ sgk6.9.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+ char line[80]; /* line[80] is an array which stores 80 characters */
+
+ scanf("%[ ABCDEFGHIJKLMNOPQRSTUVWXYZ]",line);
+ 
+}
